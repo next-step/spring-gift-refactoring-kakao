049883ec98fb276cb4a60084cafc7c7f3dfe39aa
sapiensxxv <jhspacelover@naver.com>
2026-02-26 13:02:12 +0900

    style: Entity 필드 사이 빈 줄 패턴 통일

--- src/main/java/gift/member/Member.java
+++ src/main/java/gift/member/Member.java
@@ -10,13 +10,9 @@ public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String email;
-
     private String password;
-
     private String kakaoAccessToken;
-
     private int point;
 
     protected Member() {
