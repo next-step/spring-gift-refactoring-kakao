1106fe05aa192024c35d07744a1d6886aca6b47e
jbw9964 <jbw9964@gmail.com>
2026-02-27 18:17:38 +0900

    chore: 인수 테스트를 위한 rest assured, cucumber 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,12 @@ dependencies {
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation(platform("io.cucumber:cucumber-bom:7.20.1"))
+    testImplementation("io.cucumber:cucumber-java")
+    testImplementation("io.cucumber:cucumber-spring")
+    testImplementation("io.cucumber:cucumber-junit-platform-engine")
+    testImplementation("io.rest-assured:rest-assured:5.5.0")
+    testImplementation("org.junit.platform:junit-platform-suite")
 
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
