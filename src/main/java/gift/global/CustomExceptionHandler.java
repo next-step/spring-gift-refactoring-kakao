c8b506b42b4f76c42fcd614447f779736b5cc442
jbw9964 <jbw9964@gmail.com>
2026-03-02 02:18:33 +0900

    refactor: exception handler 에 validation ex 추가

--- src/main/java/gift/global/CustomExceptionHandler.java
+++ src/main/java/gift/global/CustomExceptionHandler.java
@@ -1,7 +1,12 @@
 package gift.global;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,5 +22,31 @@ public class CustomExceptionHandler {
         return ResponseEntity
                 .status(httpStatus)
                 .body(message);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidation(MethodArgumentNotValidException e) {
+
+        Map<String, List<String>> errors = new HashMap<>();
+
+        e.getBindingResult().getFieldErrors().forEach(fieldError -> {
+            String field = fieldError.getField();
+            String message = fieldError.getDefaultMessage();
+
+            if (!errors.containsKey(field)) {
+                errors.put(field, new ArrayList<>());
+            }
+
+            errors.get(field).add(message);
+        });
+
+        ValidationErrorResponse response = new ValidationErrorResponse(
+                "Bad request: validation failed",
+                errors
+        );
+
+        return ResponseEntity
+                .badRequest()
+                .body(response);
     }
 }
