e184ac4bec425e05464136dfe810f4b07cb8b88c
jbw9964 <jbw9964@gmail.com>
2026-03-02 00:36:43 +0900

    refactor: 불필요한 jpa query method 제거

--- src/main/java/gift/member/internal/MemberRepository.java
+++ src/main/java/gift/member/internal/MemberRepository.java
@@ -12,8 +12,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    Optional<Member> findByEmail(String email);
-
     Optional<Member> findByEmailAndPassword(String email, String password);
 
     boolean existsByEmail(String email);
