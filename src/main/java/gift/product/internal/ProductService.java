a5151323fb1c9678f8999f07c9b475ed65c64e0d
jbw9964 <jbw9964@gmail.com>
2026-03-02 19:47:00 +0900

    feat: product service 구성

---
+++ src/main/java/gift/product/internal/ProductService.java
@@ -0,0 +1,83 @@
+package gift.product.internal;
+
+import gift.category.Category;
+import gift.global.NotFoundException;
+import gift.product.Product;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedModel;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepo;
+    private final ProductCategoryRepository categoryRepo;
+
+    public PagedModel<ProductResponse> getProducts(Pageable pageable) {
+        Page<ProductResponse> pageResponse = productRepo.findAll(pageable)
+                .map(ProductResponse::from);
+
+        return new PagedModel<>(pageResponse);
+    }
+
+    public ProductResponse getProduct(Long productId) {
+        Product find = productRepo.findById(productId)
+                .orElseThrow(NotFoundException::productNotFound);
+
+        return ProductResponse.from(find);
+    }
+
+    @Transactional
+    public ProductResponse createProduct(ProductRequest createRequest) {
+        Long categoryId = createRequest.categoryId();
+
+        Category category = categoryRepo.findById(categoryId)
+                .orElseThrow(NotFoundException::categoryNotFound);
+
+        String name = createRequest.name();
+        int price = createRequest.price();
+        String imageUrl = createRequest.imageUrl();
+
+        Product build = Product.builder()
+                .category(category)
+                .name(name)
+                .price(price)
+                .imageUrl(imageUrl)
+                .build();
+
+        Product newEntity = productRepo.save(build);
+
+        return ProductResponse.from(newEntity);
+    }
+
+    @Transactional
+    public ProductResponse updateProduct(Long productId, ProductRequest updateRequest) {
+        Long categoryId = updateRequest.categoryId();
+
+        Category category = categoryRepo.findById(categoryId)
+                .orElseThrow(NotFoundException::categoryNotFound);
+
+        Product find = productRepo.findById(productId)
+                .orElseThrow(NotFoundException::productNotFound);
+
+        String name = updateRequest.name();
+        int price = updateRequest.price();
+        String imageUrl = updateRequest.imageUrl();
+
+        find.update(name, price, imageUrl, category);
+
+        return ProductResponse.from(find);
+    }
+
+    @Transactional
+    public void deleteProduct(Long productId) {
+        Product find = productRepo.findById(productId)
+                .orElseThrow(NotFoundException::productNotFound);
+
+        productRepo.delete(find);
+    }
+}
