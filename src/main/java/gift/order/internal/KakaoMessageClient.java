dee295438ada6e9f34b2cc66a724a4b5ed8c789a
jbw9964 <jbw9964@gmail.com>
2026-03-02 15:20:30 +0900

    refactor: KakaoMessageClient 가 메세지 전송만을 담당하도록 재구성

--- src/main/java/gift/order/internal/KakaoMessageClient.java
+++ src/main/java/gift/order/internal/KakaoMessageClient.java
@@ -1,7 +1,5 @@
 package gift.order.internal;
 
-import gift.order.Order;
-import gift.product.Product;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.client.RestClient;
@@ -15,39 +13,17 @@ public class KakaoMessageClient {
         this.restClient = builder.build();
     }
 
-    public void sendToMe(String accessToken, Order order, Product product) {
-        var templateObject = buildTemplate(order, product);
+    public void sendDefaultTemplateMessageToMe(String kakaoAccessToken, String templateObject) {
 
-        var params = new LinkedMultiValueMap<String, String>();
-        params.add("template_object", templateObject);
+        LinkedMultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
+        parameters.add("template_object", templateObject);
 
         restClient.post()
                 .uri("https://kapi.kakao.com/v2/api/talk/memo/default/send")
-                .header("Authorization", "Bearer " + accessToken)
+                .header("Authorization", "Bearer " + kakaoAccessToken)
                 .header("Content-Type", "application/x-www-form-urlencoded")
-                .body(params)
+                .body(parameters)
                 .retrieve()
                 .toBodilessEntity();
-    }
-
-    private String buildTemplate(Order order, Product product) {
-        var totalPrice = String.format("%,d", product.getPrice() * order.getQuantity());
-        var message = order.getMessage() != null && !order.getMessage().isBlank()
-                ? "\\n\\n💌 " + order.getMessage()
-                : "";
-        return """
-                {
-                    "object_type": "text",
-                    "text": "🎁 선물이 도착했어요!\\n\\n%s (%s)\\n수량: %d개\\n금액: %s원%s",
-                    "link": {},
-                    "button_title": "선물 확인하기"
-                }
-                """.formatted(
-                product.getName(),
-                order.getOption().getName(),
-                order.getQuantity(),
-                totalPrice,
-                message
-        );
     }
 }
