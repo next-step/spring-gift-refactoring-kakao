66d41fd507904f818bd0963dec5dd026056c9258
seungwone <aahhll654@gmail.com>
2026-03-03 11:39:56 +0900

    refactor: MemberService.register()에서 create()를 재사용하여 중복 로직 제거

--- src/main/java/gift/member/MemberService.java
+++ src/main/java/gift/member/MemberService.java
@@ -18,10 +18,7 @@ public class MemberService {
     }
 
     public TokenResponse register(MemberRequest request) {
-        if (memberRepository.existsByEmail(request.email())) {
-            throw new IllegalArgumentException("Email is already registered.");
-        }
-        Member member = memberRepository.save(new Member(request.email(), request.password()));
+        Member member = create(request.email(), request.password());
         String token = jwtProvider.createToken(member.getEmail());
         return new TokenResponse(token);
     }
