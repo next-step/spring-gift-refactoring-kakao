b0d6f5796ff797e527ce8a1615d543760b57e5f5
Deun Lee <deunlee@deunlee.com>
2026-03-03 18:12:48 +0900

    chore(db): 기존 회원 비밀번호 BCrypt 마이그레이션 추가

---
+++ src/main/resources/db/migration/V3__Encrypt_member_passwords.sql
@@ -0,0 +1,3 @@
+update member set password = '$2a$10$ixdpyWsLpHgGqoOPNoI/0.J2jyuLs9Z90r4Cji3PvKVhMNkmUA1ZG' where email = 'admin@example.com';
+update member set password = '$2a$10$PInqze2lGegO/nIWUom25udFasQ.vYOsuhI9XqEWzRX6eWUlJE9J.' where email = 'user1@example.com';
+update member set password = '$2a$10$bUgNPA0Ac7aDjrJ6X2kL8OAZvabN69eWZecD5sAwoTkFzQycu92w6' where email = 'user2@example.com';
