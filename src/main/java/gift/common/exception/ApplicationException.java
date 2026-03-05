7cbb9826a94892bdc8a95dcfde0049eb7a334123
semi-colon-00 <semi.colon@kakaocorp.com>
2026-03-05 14:31:56 +0900

    feat: 커스텀 예외 인프라 구성 (ErrorCode, ApplicationException, GlobalExceptionHandler)

---
+++ src/main/java/gift/common/exception/ApplicationException.java
@@ -0,0 +1,13 @@
+package gift.common.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ApplicationException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public ApplicationException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
