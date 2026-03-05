a26edbd5206ecd574b1ea9df49451e21abbb6530
jbw9964 <jbw9964@gmail.com>
2026-03-05 18:08:19 +0900

    refactor: OrderService 가 OptionQuery, Command, MemberCommandPort 를 사용하도록 재구성

--- src/main/java/gift/order/internal/OrderService.java
+++ src/main/java/gift/order/internal/OrderService.java
@@ -1,9 +1,11 @@
 package gift.order.internal;
 
-import gift.global.NotFoundException;
+import gift.member.MemberCommandPort;
 import gift.option.Option;
+import gift.option.OptionCommandPort;
+import gift.option.OptionQueryPort;
 import gift.order.Order;
-import gift.product.Product;
+import gift.product.ProductDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,8 +18,9 @@ import org.springframework.transaction.annotation.Transactional;
 public class OrderService {
 
     private final OrderRepository orderRepo;
-    private final OrderOptionRepository optionRepo;
-    private final OrderMemberRepository memberRepo;
+    private final OptionQueryPort optionQueryPort;
+    private final OptionCommandPort optionCommandPort;
+    private final MemberCommandPort memberCommandPort;
 
     public PagedModel<OrderResponse> getOrders(Long memberId, Pageable pageable) {
         Page<OrderResponse> pageResponse = orderRepo.findByMemberId(memberId, pageable)
@@ -32,24 +35,18 @@ public class OrderService {
         int quantity = request.quantity();
         String message = request.message();
 
-        // validate option
-        Option option = optionRepo.findByIdInnerJoinFetchProduct(optionId)
-                .orElseThrow(NotFoundException::optionNotFound);
-
         // subtract stock
-        option.subtractQuantity(quantity);
-
-        Product product = option.getProduct();
+        optionCommandPort.subtractQuantity(optionId, quantity);
 
         // deduct points
-        int price = product.getPrice() * quantity;
-        memberRepo.findById(memberId)
-                .orElseThrow(NotFoundException::memberNotFound)
-                .deductPoint(price);
+        ProductDto product = optionQueryPort.getAssociatedProduct(optionId);
+        int price = product.price() * quantity;
+        memberCommandPort.deductPoint(memberId, price);
 
         // save order
+        Option optionRef = optionQueryPort.getReference(optionId);
         Order build = Order.builder()
-                .option(option)
+                .option(optionRef)
                 .memberId(memberId)
                 .quantity(quantity)
                 .message(message)
