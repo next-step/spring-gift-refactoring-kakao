33520ab2612353391d28bbdd665ed9c6dce915a2
jbw9964 <jbw9964@gmail.com>
2026-03-05 21:11:08 +0900

    feat: 옴션을 수정하는 endpoint 활성화

--- src/main/java/gift/option/internal/OptionController.java
+++ src/main/java/gift/option/internal/OptionController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,6 +47,18 @@ public class OptionController {
         return ResponseEntity
                 .created(location)
                 .body(response);
+    }
+
+    @PutMapping("/{optionId}")
+    public ResponseEntity<OptionResponse> updateOption(
+            @PathVariable Long productId,
+            @PathVariable Long optionId,
+            @Valid @RequestBody OptionRequest request
+    ) {
+        OptionResponse response = optionService.updateOption(productId, optionId, request);
+
+        return ResponseEntity
+                .ok(response);
     }
 
     @DeleteMapping(path = "/{optionId}")
