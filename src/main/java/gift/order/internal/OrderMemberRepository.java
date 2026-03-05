8bd2c653623f7a829de8272274df82bbacf22f03
jbw9964 <jbw9964@gmail.com>
2026-03-05 18:10:31 +0900

    refactor: 사용하지 않는 jpa repository 제거

--- src/main/java/gift/order/internal/OrderMemberRepository.java
+++
@@ -1,8 +0,0 @@
-package gift.order.internal;
-
-import gift.member.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderMemberRepository extends JpaRepository<Member, Long> {
-
-}
